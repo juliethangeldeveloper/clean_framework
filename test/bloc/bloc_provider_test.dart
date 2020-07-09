83d2ac8c284995e2c2f38b6136139346d7c54a39
sarbagyastha <sarbagyastha@gmail.com>
2020-07-09 18:13:05 +0545

    added tests

--- test/bloc/bloc_provider_test.dart
+++ test/bloc/bloc_provider_test.dart
@@ -38,7 +38,7 @@ void main() {
     await tester.pumpWidget(widget);
     await tester.pump(Duration.zero);
 
-    expect(Provider.of<TestBloc>(_context, listen: false).blocId, 2);
+    expect(BlocProvider.of<TestBloc>(_context).blocId, 2);
   });
 
   testWidgets(
@@ -58,11 +58,32 @@ void main() {
     await tester.pumpWidget(widget);
     await tester.pump(Duration.zero);
 
-    expect(Provider.of<TestBloc>(_context, listen: false).blocId, 1);
+    expect(BlocProvider.of<TestBloc>(_context).blocId, 1);
+  });
+
+  testWidgets('bloc provider test with extension', (tester) async {
+    BuildContext _context;
+    final widget = Provider<TestBloc>(
+      create: (_) => TestBloc(2),
+      child: BlocProvider(
+        create: (_) => TestBloc(1),
+        child: Builder(
+          builder: (context) {
+            _context = context;
+            return Container();
+          },
+        ),
+      ),
+    );
+
+    await tester.pumpWidget(widget);
+    await tester.pump(Duration.zero);
+
+    expect(_context.bloc<TestBloc>().blocId, 2);
   });
 
   // Only for comparing with behaviour of bloc provider
-  testWidgets('provider test with "of"', (tester) async {
+  testWidgets('provider test', (tester) async {
     BuildContext _context;
     final widget = Provider<TestBloc>(
       create: (_) => TestBloc(2),
@@ -81,26 +102,5 @@ void main() {
     await tester.pump(Duration.zero);
 
     expect(Provider.of<TestBloc>(_context, listen: false).blocId, 1);
-  });
-
-  testWidgets('provider test with extension', (tester) async {
-    BuildContext _context;
-    final widget = Provider<TestBloc>(
-      create: (_) => TestBloc(2),
-      child: Provider(
-        create: (_) => TestBloc(1),
-        child: Builder(
-          builder: (context) {
-            _context = context;
-            return Container();
-          },
-        ),
-      ),
-    );
-
-    await tester.pumpWidget(widget);
-    await tester.pump(Duration.zero);
-
-    expect(_context.bloc<TestBloc>().blocId, 1);
   });
 }
